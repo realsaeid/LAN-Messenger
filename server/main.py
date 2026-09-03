baa352f8bac866c56d00af86902e7b2294c61c0e
realsaeid <naghipoursaeid@gmail.com>
2026-09-02 18:15:56 -0700

    create FastAPI server

--- server/main.py
+++ server/main.py
@@ -1,1 +1,18 @@
-print("Hello welocome to my app")
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="LAN-Messenger", 
+    description="Local Network Messaging Server",
+    version="1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "massege":"Lan Massneger Server is running"
+    }
+@app.get("/health")
+def health_check():
+    return{
+        "stutus":"ok"
+    }
